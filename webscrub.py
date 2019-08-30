bf74e3ec220124b0b634216e80096aef538dc344
Zach Brookler <zachbrookler@Zachs-MacBook-Pro-2.local>
2019-08-30 15:31:32 -0400

    Added necessary imports

---
+++ webscrub.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+import re
+import inspect
